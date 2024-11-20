4cd5841776f75dac8e469c54a2b08e30d8e7f365
Daniela <13044@portalcedi.edu.mx>
2024-11-20 08:40:21 -0600

    reloj añadido

--- reloj.php
+++ reloj.php
@@ -6,6 +6,25 @@
     <title>reloj</title>
 </head>
 <body>
+
+<div id="clock"><?php echo date('H:i:s'); ?></div>
+
+    <script>
+        function startClock() {
+            const clock = document.getElementById('clock');
+
+            setInterval(() => {
+                const ahora = new Date();
+                const horas = String(ahora.getHours()).padStart(2, '0');
+                const minutos = String(ahora.getMinutes()).padStart(2, '0');
+                const segundos = String(ahora.getSeconds()).padStart(2, '0');
+                
+                clock.textContent = `${horas}:${minutos}:${segundos}`;
+            }, 1000);
+        }
+
+        window.onload = startClock;
+    </script>
     
 </body>
 </html>
