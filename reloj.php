b7b7759205d2d0c614adbc1ce53ceb34c5660332
Daniela <13044@portalcedi.edu.mx>
2024-11-21 10:43:14 -0600

    arreglar error al recargar pag

--- reloj.php
+++ reloj.php
@@ -33,39 +33,48 @@
         margin-top: 100px;
         }
 </style>
+
 <body>
 
-<center><h1 class="display-1">
-<div id="clock" font-family="Kbzipadeedoodah"><?php echo date('H:i:s'); ?></div>
-</h1><h1>
-<p id="texto">Texto</p>
-</center></h1>
-<script>
-    function startClock() {
-        const clock = document.getElementById('clock');
-        var parrafo = document.getElementById('texto');
+    <center>
+        <h1 class="display-1">
+            <div id="clock" font-family="Kbzipadeedoodah"></div>
+        </h1>
+        <h1>
+            <p id="texto"></p>
+        </h1>
+    </center>
 
-        setInterval(() => {
-            const ahora = new Date();
-            const horas = ahora.getHours(); // Devuelve un número
-            const minutos = String(ahora.getMinutes()).padStart(2, '0');
-            const segundos = String(ahora.getSeconds()).padStart(2, '0');
-            
-            clock.textContent = `${String(horas).padStart(2, '0')}:${minutos}:${segundos}`;
-            
-            // Cambiar el mensaje según la hora
-            if (horas >= 6 && horas < 12) {
-                parrafo.innerHTML = "Buenos días";
-            } else if (horas >= 12 && horas < 18) {
-                parrafo.innerHTML = "Buenas tardes";
-            } else {
-                parrafo.innerHTML = "Buenas noches";
+    <script>
+        function startClock() {
+            const clock = document.getElementById('clock');
+            var parrafo = document.getElementById('texto');
+
+            function actualizarHora() {
+                const ahora = new Date();
+                const horas = ahora.getHours(); // Devuelve un número
+                const minutos = String(ahora.getMinutes()).padStart(2, '0');
+                const segundos = String(ahora.getSeconds()).padStart(2, '0');
+                
+                clock.textContent = `${String(horas).padStart(2, '0')}:${minutos}:${segundos}`;
+                
+                // Cambiar el mensaje según la hora
+                if (horas >= 6 && horas < 12) {
+                    parrafo.innerHTML = "Buenos días";
+                } else if (horas >= 12 && horas < 18) {
+                    parrafo.innerHTML = "Buenas tardes";
+                } else {
+                    parrafo.innerHTML = "Buenas noches";
+                }
             }
-        }, 1000);
-    }
 
-    window.onload = startClock;
-</script>
+            actualizarHora();
+            setInterval(actualizarHora, 1000);
+
+        }
+
+        window.onload = startClock;
+    </script>
 
 </body>
 
